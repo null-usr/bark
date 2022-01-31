e904b2f940d05d10e6585ba4533f7a78f7856ddc
null-usr <40791870+null-usr@users.noreply.github.com>
2022-01-31 22:42:00 +0100

    Palette (#22)
    * feat: drag and drop example works
    
    * feat(palette): node icons in sidebar & more reasonable colour
    
    * feat: add dialogue palette item
    
    * chore: stop linter from shitting itself
    
    * chore: delete header.tsx
    
    * chore: cleaing up my mess
    
    * feat: base node added to palette
    
    Co-authored-by: Tristan Villamil <tristanvillamil@Tristans-MacBook-Air.local>

--- dialogue-editor-client/src/components/page/workspace/palette/styles.ts
+++ dialogue-editor-client/src/components/page/workspace/palette/styles.ts
@@ -4,7 +4,26 @@ export const PaletteContainer = styled.div`
     // height: 100%;
     display: flex;
     flex-direction: column;
-    background: yellow;
+    justify-content: centered;
 
     padding: 20px;
+
+    border-right: 1px solid #eee;
+    padding: 15px 10px;
+    font-size: 12px;
+    background: #fcfcfc;
+
+    @media screen and (min-width: 768px) {
+        //width: 20%;
+        //max-width: 180px;
+    }
+
+    .node {
+        margin-bottom: 10px;
+        cursor: grab;
+    }
+
+    .description {
+        margin-bottom: 10px;
+    }
 `
