652bf2e9580da6ddcf0458806f0ef31f4bc79195
neil <ntclarke33@gmail.com>
2024-06-12 19:46:36 +0200

    feat: trying to get electron to build something

---
+++ electron.vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
+import { resolve } from 'path'
+
+export default defineConfig({
+	main: {
+		build: {
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, 'electron/main/main.ts'),
+				},
+			},
+		},
+	},
+	preload: {
+		build: {
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, 'electron/preload/preload.ts'),
+				},
+			},
+		},
+	},
+	renderer: {
+		root: '.',
+		plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+		build: {
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, 'index.html'),
+					about: resolve(__dirname, 'about.html'),
+				},
+			},
+		},
+	},
+})
---
+++ src/AboutPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+// overwrite default node stylings, see "Theming" on docs page
+import '@/helpers/style.css'
+
+function App() {
+	return <div className="About">TEST</div>
+}
+
+export default App
